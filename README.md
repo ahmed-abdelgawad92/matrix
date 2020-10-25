917e8702d82e9ea5b356735ade7c85ddc5ee514c
Ahmed Abdelgawad <ahmed.elkayaty92@gmail.com>
2020-10-25 16:25:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Matrix Multiplication
+
+is a Laravel / Vue App that takes 2 matrices as input and multiply these 2 matrices and return an encoded Matrix as a result with the ability to decode it.
+
+## App Setup
+
+- Execute first the following command to install all the backend depenencies
+``` 
+composer install 
+```
+- Then install all the frontend dependencies using
+````
+npm install
+````
+- Now build the frontend files using the command
+````
+npm run dev
+````
+- Run the app with the following laravel php artisan command
+```
+php artisan serve
+```
+
---
+++ webpack.mix.js
@@ -0,0 +1,26 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .babelConfig({
+        plugins: ['@babel/plugin-syntax-dynamic-import'],
+    })
+    .webpackConfig({
+        resolve: {
+            extensions: ['.js', '.json', '.vue'],
+            alias: {
+                '@': path.resolve(__dirname, './resources/js')
+            }
+        },
+    })
+    .sass('resources/sass/app.scss', 'public/css');
